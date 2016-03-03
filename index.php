119e69165f6a795e1b2d9a643af78df06c516055
Jimmylet <jimmy.letecheur@student.hepl.be>
2016-03-03 13:51:19 +0100

    establishing database connection

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * index.php
+ * Créé par : Jimmy Letecheur
+ * Date : 3/03/16
+ */
+
+
+$cn = new PDO('mysql:dbname=exercice_php_biblio;host=localhost', 'root','root');
+/*variable pour la connexion, dans lequel on instancie un objet pdo. On lui donne le nom de la base de donnée mysql. On lui donne
+le nom d'utilisateur et le mot de passe.*/
+
+
+
+?>
